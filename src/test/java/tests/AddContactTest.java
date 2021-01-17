bc2ee38e9a2c7985a33ba975190fe30f9112fda3
koluvola <34248009+koluvola@users.noreply.github.com>
2021-01-17 20:23:21 +0200

    changed bieldGradle and TestBase

--- src/test/java/tests/AddContactTest.java
+++ src/test/java/tests/AddContactTest.java
@@ -1,12 +1,13 @@
 package tests;
 
+import model.Contact;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AddContactTest extends TestBase {
     String contactName = "Alex";
-    int i = (int) ((System.currentTimeMillis() / 1000) / 3600);
+    int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
     private String address = "Tel Aviv " + i;
     private String description = "New contact " + i;
     private String email = "test1234A447266@mail.ru";
@@ -18,16 +19,16 @@ public class AddContactTest extends TestBase {
     public void addContactTest() {
 
         app.getContact().clickAddButton();
-        //app.getContact().fillAddContactForm(new Contact().withEmail(email).withAddress(address).withDescription(description).withlName(lName).withName(name).withPhone(phone));
+        app.getContact().fillAddContactForm(new Contact().withEmail(email).withAddress(address).withDescription(description).withlName(lName).withName(name).withPhone(phone));
         //String name, String Lname, String phone, String email, String address, String descript
-        app.getContact().fillAddContactFormNew(name, lName, phone, email, address, description);
+        //app.getContact().fillAddContactFormNew(name, lName, phone, email, address, description);
         app.getContact().clickSaveButton("//button[contains(.,'Save')]");
         app.getContact().openContacts();
         app.getContact().delay(4000);
 
         Assert.assertTrue(app.getContact().isNameExist(contactName));
 
-        app.getContact().clearContacts();
+       // app.getContact().clearContacts();
         app.getContact().delay(10000);
 
     }
